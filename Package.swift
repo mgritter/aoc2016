73ea47538a5965337b24d6640f21ac348e914007
Mark Gritter <mgritter@gmail.com>
2016-12-11 15:01:59 -0800

    Added day2 program.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
   name: "aoc2016",
   targets: [
-    Target( name: "day1", dependencies: [ "SimpleFile" ] )
+    Target( name: "day1", dependencies: [ "SimpleFile" ] ),
+    Target( name: "day2", dependencies: [ "SimpleFile" ] )
   ]
 )
